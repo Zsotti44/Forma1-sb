a834aedd6fe635eef95a4f92a80b54d4a104ecbb
Katona Zsolt <zsotti44@gmail.com>
2024-10-03 19:47:47 +0200

    .

---
+++ src/main/java/com/f1sb/forma1sb/Forma1SbApplication.java
@@ -0,0 +1,13 @@
+package com.f1sb.forma1sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Forma1SbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Forma1SbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/f1sb/forma1sb/Forma1SbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1sb.forma1sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Forma1SbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
